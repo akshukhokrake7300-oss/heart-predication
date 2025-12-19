2ddfc671e220e0d3a3410e9a5bf46341c153b5a1
akshukhokrake7300-oss <akshukhokrake7300@gmail.com>
2025-12-19 11:51:06 +0530

    updated file
    Removed MongoDB connection setup from database.py.

--- src/database.py
+++ src/database.py
@@ -1,11 +1,4 @@
 FLASK_PORT_NUMBER=5002
-import pymongo
-
-mongo_url="mongodb://localhost:27017"
-mongo_client= pymongo.MongoClient(mongo_url)
-
-database='prediction'
-db=mongo_client[database]
 import pickle
 import pandas as pd
 import json
